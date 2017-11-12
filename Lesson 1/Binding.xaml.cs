06f1fca786a889877092edc5db9b4b97259e8ffa
kszczurowski <kszczurowski@absolute.com>
2017-11-11 21:03:57 -0800

    Finished bindind RichTextBox to Menu controls; Clean up the project, removed generic canvas name, corrected references, changes object initialization, removed unnecessary using statements;

---
+++ Lesson 1/Binding.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Lesson_1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// Name: Krzysztof Szczurowski
+    /// Week 3 Lab 1
+    /// Repo address: https://github.com/kriss3/BCIT_WPF_COMP3608_Week3Lab1.git
+    /// </summary>
+    public partial class Binding : Window
+    {
+        public Binding()
+        {
+            InitializeComponent();
+            foreach (FontFamily myFont in Fonts.SystemFontFamilies)
+            {
+                ListBoxItem l = new ListBoxItem
+                {
+                    Content = myFont.ToString(),
+                    FontFamily = myFont
+                };
+                listBox1.Items.Add(l);
+            }
+        }
+    }
+}
